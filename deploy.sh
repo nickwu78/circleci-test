c4334c299232925862b320fd7d901408f1dad753
Nick Wu <nickwu78@gmail.com>
2017-07-28 16:12:51 +1000

    circleci

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 export AWS_ACCESS_KEY=${AWS_ACCESS_KEY_DEPLOY}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_DEPLOY}
-echo "Environment vars set, running the gulpfile"
+echo "PRODUCTION: Environment vars set, running the gulpfile"
 NODE_ENV=production node gulpfile.js
--- staging.sh
+++ staging.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export AWS_ACCESS_KEY=${AWS_ACCESS_KEY_STAGE}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_STAGE}
-echo "Environment vars set, running the gulpfile"
+echo "STAGING: Environment vars set, running the gulpfile"
 NODE_ENV=development node gulpfile.js

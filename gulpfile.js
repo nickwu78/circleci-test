ea6c085bd973a8b610d1d290959793b63e51c50e
Nick Wu <nickwu78@gmail.com>
2017-07-28 15:52:05 +1000

    circleci test

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,1 @@
-console.log("Hi, i'm a gulpfile, environment is: " + process.env.NODE_ENV + " key is: " + process.env.TESTKEY)
+console.log("Hi, i'm a gulpfile, environment is: " + process.env.NODE_ENV + " key is: " + TESTKEY)
--- set_deploy_env.sh
+++ set_deploy_env.sh
@@ -1,4 +1,4 @@
 TESTKEY=${AWS_ACCESS_KEY_DEPLOY}
 echo $TESTKEY
 echo "Starting the gulpfile"
-node gulpfile.js
+NODE_ENV=test node gulpfile.js
